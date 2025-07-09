46c229a64de0c69834c53f6c57fcb5fe063c6c53
gianpool <gianpoolalvinocantaro@gmail.com>
2025-07-09 08:54:35 -0500

    repasando los getters setters y this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,14 +1,25 @@
 export class Pokemon{
-    
-    public id:number;
-    public name:string;
 
-    constructor(id:number,name:string){
-        this.id=id;
-        this.name=name;
-        console.log('constructo llamado');
+    get imageUrl():string{
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
+    constructor(
+        public readonly id: number,
+        public name:string,
+        //public imageURL: string,
+    ) {}
+
+    scream(){
+        console.log(`${this.name.toUpperCase()} !!!`);
+    }
+
+    speak(){
+        console.log(`${this.name},${this.name}`);
     }
 
 }
 
 export const charmander=new Pokemon(4,'Charmander')
+charmander.scream();
+charmander.speak();
